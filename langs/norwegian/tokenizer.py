833e3575c99ad5ead7c186aa25c1992f32194773
Amar Lakshya <amar.lakshya@protonmail.com>
2022-02-28 12:00:52 +0000

    renaming of lessons

--- langs/norwegian/tokenizer.py
+++ langs/norwegian/tokenizer.py
@@ -2,16 +2,13 @@ import sys
 import re
 
 KEYWORDS = ['amar', 'ane']
-sys.argv[0] = "./lesson1-2"
-if(not sys.argv[0]):
+if(len(sys.argv) == 1):
     sys.exit()
 
-f = open(sys.argv[0])
+f = open(sys.argv[1])
 
 words = set(re.sub(r'[^\w\s]','',''.join(f.readlines()).lower()).format(*KEYWORDS).split())
 for keyword in KEYWORDS:
-        words.remove(keyword)
-print(words)
+        words.discard(keyword)
+print(sorted(words))
 print(len(words))
-
-
